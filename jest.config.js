62385371ed892e554b8e8f0c300af637037ca7e7
ieeebruce <ieeebruce@foxmail.com>
2025-12-03 22:47:18 +0800

    1

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/scripts/i18n-refactor/tests'],
-  modulePaths: ['<rootDir>/scripts/i18n-refactor/src'],
+  roots: ['<rootDir>/i18n-refactor/tests'],
+  modulePaths: ['<rootDir>/i18n-refactor/src'],
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/scripts/i18n-refactor/tsconfig.json'
+      tsconfig: '<rootDir>/i18n-refactor/tsconfig.json'
     }
   }
 }
