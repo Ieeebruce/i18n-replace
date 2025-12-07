a7ff498ca7ea6d20064b03ac37bf7ea9ab8e96d9
ieeebruce <ieeebruce@foxmail.com>
2025-12-08 00:02:26 +0800

    1

--- i18n-refactor/tests/runner/component-basic.spec.ts
+++ i18n-refactor/tests/runner/component-basic.spec.ts
@@ -15,3 +15,35 @@ test('basic: L alias with simple property', () => {
   expect(out.tsOut).toContain(`this.i18n.get('app.title')`)
   expect(out.htmlOut).toContain(`{{ 'app.title' | i18n }}`)
 })
+
+test('basic: L alias with simple property', () => {
+  const ts = `class C { 
+  L: any;
+  constructor(private locale: I18nLocaleService) { 
+    this.L = this.locale.getLocale().app
+  } 
+  ngOnInit() { 
+    console.log(this.L.title) 
+  } 
+}`
+  const html = `<h1>{{ L.title }}</h1>`
+  const out = processComponent(ts, html)
+  expect(out.tsOut).toContain(`this.i18n.get('app.title')`)
+  expect(out.htmlOut).toContain(`{{ 'app.title' | i18n }}`)
+})
+
+test('basic: L alias with simple property', () => {
+  const ts = `class C { 
+  L: any;
+  constructor(private locale: I18nLocaleService) { 
+    this.L = this.locale.getLocale().app.common
+  } 
+  ngOnInit() { 
+    console.log(this.L.desc) 
+  } 
+}`
+  const html = `<h1>{{ L.desc }}</h1>`
+  const out = processComponent(ts, html)
+  expect(out.tsOut).toContain(`this.i18n.get('app.common.desc')`)
+  expect(out.htmlOut).toContain(`{{ 'app.common.desc' | i18n }}`)
+})
